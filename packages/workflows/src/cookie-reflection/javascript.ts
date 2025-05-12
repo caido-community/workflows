7acee45c30662a22a27ede9e3c73134b0f821d48
meme-lord <meme-lord@users.noreply.github.com>
2025-05-12 13:36:52 +0100

    updated cookie reflection workflow to have a better title

--- packages/workflows/src/cookie-reflection/javascript.ts
+++ packages/workflows/src/cookie-reflection/javascript.ts
@@ -5,28 +5,24 @@
  */
 export async function run({ request, response }, sdk) {
     if (request && response) {
-      // get cookie header value
-      // parse cookie header to get values
-      // check response for the cookie value (if length of cookie is > 5)
-      // create finding
       const cookie_header = request.getHeader('Cookie');
       if(cookie_header) {
         const split = cookie_header[0].split(";");
-        for(let i = 0; i < split.length; i++){
-          const split2 = split[i].split('=');
+        for(const cookie of split){
+          const split2 = cookie.split('=');
           const cookie_name = split2[0];
           const cookie_val = split2[1];
           if(cookie_val.length < 6){
+            // checking short cookie values like "false" would create many false positives
             continue;
           }
           if(response.getBody().toText().indexOf(cookie_val) != -1){
-            const description = `The request to ${request.getUrl()} has contains the value of cookie ${cookie_name}`;
             await sdk.findings.create({
-              title: "Cookie Value Reflected In Response",
-              description: description,
+              title: `Value "${cookie_val}" from cookie "${cookie_name}" is reflected`,
+              description: `The response from ${request.getUrl()} contains the value "${cookie_val}" which is also the value of the cookie "${cookie_name}"`,
               request: request,
               reporter: "CookieValueReflectedInResponse",
-              dedupeKey: description
+              dedupeKey: `cookie_reflect_${request.getUrl()}_${cookie_name}_${cookie_val}`
             });
           }
         }
