5852f9356d7a3d0d531d9a8ced4065022d610662
meme-lord <meme-lord@users.noreply.github.com>
2025-06-29 14:12:08 +0100

    reduced FPs from json with wrong content type workflow

--- packages/workflows/src/json-with-wrong-content-type/javascript.ts
+++ packages/workflows/src/json-with-wrong-content-type/javascript.ts
@@ -11,14 +11,16 @@ export async function run({ request, response }, sdk) {
         if (body.startsWith('{') || body.startsWith('[')) {
             // Regex to ignore application/json application/manifest+json 
             if (!contentTypeHeader[0].match(/^application\/(\w*\+)?json/)) {
-                let description = `The content of response from ${request.getHost()}${request.getPath()} is probably JSON but the content type is not application/json`;
-                await sdk.findings.create({
-                title: "JSON Response Without JSON Content-Type",
-                description: description,
-                request: request,
-                reporter: "JSON Response Without JSON Content-Type",
-                dedupeKey: description
-                });
+                if (!contentTypeHeader[0].match(/^text\/(javascript|css)/)) {
+                    let description = `The content of response from ${request.getHost()}${request.getPath()} is probably JSON but the content type is not application/json`;
+                    await sdk.findings.create({
+                    title: "JSON Response Without JSON Content-Type",
+                    description: description,
+                    request: request,
+                    reporter: "JSON Response Without JSON Content-Type",
+                    dedupeKey: description
+                    });
+                }
             } 
         }
     }
