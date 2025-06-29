8edce5d599f270004b5e543f88d06cd8e227cd93
bebiksior <bebiks.priv@gmail.com>
2025-06-29 20:16:43 +0200

    fix validation errs

--- packages/workflows/src/json-with-wrong-content-type/javascript.ts
+++ packages/workflows/src/json-with-wrong-content-type/javascript.ts
@@ -8,21 +8,20 @@ export async function run({ request, response }, sdk) {
     const body = response.getBody().toText().trimLeft();
     const contentTypeHeader = response.getHeader("Content-Type");
 
-        if (body.startsWith('{') || body.startsWith('[')) {
-            // Regex to ignore application/json application/manifest+json 
-            if (!contentTypeHeader[0].match(/^application\/(\w*\+)?json/)) {
-                if (!contentTypeHeader[0].match(/^text\/(javascript|css)/)) {
-                    let description = `The content of response from ${request.getHost()}${request.getPath()} is probably JSON but the content type is not application/json`;
-                    await sdk.findings.create({
-                    title: "JSON Response Without JSON Content-Type",
-                    description: description,
-                    request: request,
-                    reporter: "JSON Response Without JSON Content-Type",
-                    dedupeKey: description
-                    });
-                }
-            } 
+    if (body.startsWith("{") || body.startsWith("[")) {
+      // Regex to ignore application/json application/manifest+json
+      if (!contentTypeHeader[0].match(/^application\/(\w*\+)?json/)) {
+        if (!contentTypeHeader[0].match(/^text\/(javascript|css)/)) {
+          let description = `The content of response from ${request.getHost()}${request.getPath()} is probably JSON but the content type is not application/json`;
+          await sdk.findings.create({
+            title: "JSON Response Without JSON Content-Type",
+            description: description,
+            request: request,
+            reporter: "JSON Response Without JSON Content-Type",
+            dedupeKey: description,
+          });
         }
+      }
     }
   }
 }
